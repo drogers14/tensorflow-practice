e94218210788759dab4af9ac9e0618910cff5076
Destiny Rogers <destinyrogers725@gmail.com>
2023-01-28 22:19:17 -0800

    initial files for building a session designed for simple addition

---
+++ addition_final.py
@@ -0,0 +1,20 @@
+import os
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+# import tensorflow as tf
+
+# Turn off Tensorflow warning messages in output
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Define computational graph
+X = tf.placeholder(tf.float32, name="X")
+Y = tf.placeholder(tf.float32, name="Y")
+
+addition = tf.add(X, Y, name="addition")
+
+# Create the session
+with tf.Session() as session:
+
+    result = session.run(addition, feed_dict={X: [1, 2, 10], Y: [4, 2, 10]})
+
+    print(result)
